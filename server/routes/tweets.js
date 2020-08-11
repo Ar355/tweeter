384870181b90267b99b514f7556f2dd7c133d762
Ar355 <zeneli.arian@gmail.com>
2020-08-11 04:28:10 +0000

    adding style to the app

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,13 +1,12 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const express = require('express');
+const userHelper = require('../lib/util/user-helper');
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const tweetsRoutes = express.Router();
 
-module.exports = function(DataHelpers) {
-
-  tweetsRoutes.get("/", function(req, res) {
+module.exports = function (DataHelpers) {
+  tweetsRoutes.get('/', function (req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -17,7 +16,7 @@ module.exports = function(DataHelpers) {
     });
   });
 
-  tweetsRoutes.post("/", function(req, res) {
+  tweetsRoutes.post('/', function (req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
@@ -29,7 +28,7 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: Date.now(),
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
