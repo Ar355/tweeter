f4be108ee856c3d2a8ae5b79459836f69302ed22
Ar355 <zeneli.arian@gmail.com>
2020-08-13 18:50:08 +0000

    add func for prevent XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,23 +5,26 @@
  */
 /// find the time sice the post
 
-
-
+/// Func for preventign SXX 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 // function for creatin a new element
 const createTweetElement = (obj) => {
-  console.log(obj.created_at);
   const date = moment (obj.created_at).fromNow();
 
   const objUsr = obj.user;
   const $tweet = `<article class='new-tweet'>
                     <header>
-                      <img src='${objUsr.avatars}'/>
-                      <span>${objUsr.name}</span>
-                      <h2>${objUsr.handle}</h2>
+                      <img src='${escape(objUsr.avatars)}'/>
+                      <span>${escape(objUsr.name)}</span>
+                      <h2>${escape(objUsr.handle)}</h2>
                     </header>
-                    <p class="tweet-text">${obj.content.text}</p>
+                    <p class="tweet-text">${escape(obj.content.text)}</p>
                     <footer>
-                      <p>${date}</p>
+                      <p>${escape(date)}</p>
                     </footer>
                   </article>`;
 
@@ -33,7 +36,7 @@ const renderTweets = function (array) {
   // eslint-disable-next-line no-restricted-syntax
   for (const element of array) {
     const $tweet = createTweetElement(element);
-    $('#tweet-container').append($tweet);
+    $('#tweet-container').prepend($tweet);
   }
 };
 
